76214ddd7f4f171883932e12789b9f0bae4f3fb9
Julia Savinkova <julsavinkova@gmail.com>
2013-12-22 19:24:27 +0200

    Use gridism

--- Mockups/Scripts/map.js
+++ Mockups/Scripts/map.js
@@ -1,12 +1,9 @@
-/**
- * Created by Savinkova on 04.12.13.
- */
 (function initialize() {
     var myOptions = {
         zoom: 8,
         center: new google.maps.LatLng(50.414778, 30.519564),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        sensor: 'true'
+        sensor: "true"
     }
 
     var map = new google.maps.Map(document.getElementById("google-container"), myOptions);
@@ -15,8 +12,4 @@
         position: new google.maps.LatLng(50.414778, 30.519564),
         map: map
     });
-    google.maps.event.addListener(marker, "click", function (e) {
-        var infoBox = new InfoBox({latlng: marker.getPosition(), map: map});
-    });
-    google.maps.event.trigger(marker, "click");
 })();
